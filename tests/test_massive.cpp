8ae46f56a09b585d97e0f413bce0b0793a3e447f
Atik <luckastey@mail.ru>
2024-10-05 17:36:54 +0300

    add tests for insert

--- tests/test_massive.cpp
+++ tests/test_massive.cpp
@@ -4,3 +4,13 @@
 #include <gtest/gtest.h>
 #include "../lib_massive/massive.h"
 
+TEST(TMassiveTest, InsertSingleValue) {
+    TMassive<int> massive;
+    massive.insert(10, 0);
+    EXPECT_FALSE(massive.empty());
+    massive.insert(20, 1);
+    massive.insert(15, 1);
+    std::stringstream ss;
+    massive.print(ss);
+    EXPECT_EQ(ss.str(), "10, 15, 20, ");
+}
