4b20f0a13b58d3ecb4c87fdc04385e36f081f1e9
Atik <luckastey@mail.ru>
2024-10-27 16:49:44 +0300

    IsEmpty Test

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -7,3 +7,9 @@ TEST(TListTest, Constructor_CreatesEmptyList) {
     TList<int> list;
     EXPECT_TRUE(list.isEmpty());
 }
+
+TEST(TListTest, IsEmpty_ReturnsFalse) {
+    TList<int> list;
+    list.push_back(1);
+    EXPECT_FALSE(list.isEmpty());
+}
