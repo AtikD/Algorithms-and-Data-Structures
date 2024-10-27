af4bd1b5f8a8e12686dd9e396afabc30f7cc5431
Atik <luckastey@mail.ru>
2024-10-27 16:36:46 +0300

    node realization

--- lib_list/node.h
+++ lib_list/node.h
@@ -35,3 +35,57 @@ class TNode {
     template <class U>
     friend std::istream& operator>>(std::istream& is, TNode<U>& node);
 };
+
+template <class T>
+TNode<T>::TNode(const T& value, TNode<T>* pnext)
+    : _value(value), _pnext(pnext) {}
+
+template <class T>
+TNode<T>::TNode(const TNode<T>& other)
+    : _value(other._value), _pnext(other._pnext) {}
+
+template <class T>
+TNode<T>& TNode<T>::operator=(const TNode<T>& other) {
+    if (this != &other) {
+        _value = other._value;
+        _pnext = other._pnext;
+    }
+    return *this;
+}
+
+template <class T>
+bool TNode<T>::operator==(const TNode<T>& other) const {
+    return _value == other._value;
+}
+
+template <class T>
+T TNode<T>::getValue() const {
+    return _value;
+}
+
+template <class T>
+void TNode<T>::setValue(const T& value) {
+    _value = value;
+}
+
+template <class T>
+TNode<T>* TNode<T>::getNext() const {
+    return _pnext;
+}
+
+template <class T>
+void TNode<T>::setNext(TNode<T>* pnext) {
+    _pnext = pnext;
+}
+
+template <class U>
+std::ostream& operator<<(std::ostream& os, const TNode<U>& node) {
+    os << node._value;
+    return os;
+}
+
+template <class U>
+std::istream& operator>>(std::istream& is, TNode<U>& node) {
+    is >> node._value;
+    return is;
+}
