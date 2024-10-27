a79f87e94f23c46947bac58c47dacb80374a1986
Atik <luckastey@mail.ru>
2024-10-27 16:51:22 +0300

    Push Front Test

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -19,3 +19,9 @@ TEST(TListTest, PushBack) {
     list.push_back(10);
     EXPECT_EQ(list.find(10)->getValue(), 10);
 }
+
+TEST(TListTest, PushFront) {
+    TList<int> list;
+    list.push_front(5);
+    EXPECT_EQ(list.find(5)->getValue(), 5);
+}
