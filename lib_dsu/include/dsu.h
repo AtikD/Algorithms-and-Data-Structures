af1353bd7a16caf973df0dae8f3c4dd574680e6c
Atik <luckastey@mail.ru>
2025-02-10 11:09:22 +0300

    added class declaration DSU(disjoint set union)

---
+++ lib_dsu/include/dsu.h
@@ -0,0 +1,17 @@
+// Copyright 2025 atikdd.t.me
+#pragma once
+
+class DSU {
+    int _size;
+    int *_parent;
+    int *rank;
+
+ public:
+    // explicit DSU(int size = 0);
+    // ~DSU();
+    // void make_set(int elem);
+    // int find(int elem);
+    // void union_trivial(int first, int second);
+    // void union_by_rank(int first, int second);
+    // void clear();
+};
---
+++ tests/test_dsu.cpp
@@ -0,0 +1,5 @@
+// Copyright 2025 atikdd.t.me
+
+#include <gtest/gtest.h>
+#include <dsu.h>
+#include <iostream>
