8b7f52b7b162be9225a6ffe3e94f91e0b65dc8d8
AtikD <luckastey@mail.ru>
2025-05-04 19:02:48 +0300

    Unsorted Table on TMassive

--- lib_massive_table/include/massive_unsorted_table.h
+++ lib_massive_table/include/massive_unsorted_table.h
@@ -1,3 +1,75 @@
 // Copyright 2025 atikdd.ru
 
 #pragma once
+
+#include <itable.h>
+#include <massive.h>
+#include <pair.h>
+
+template <typename TKey, typename TVal>
+class TMassiveUnsortedTable : public ITable<TKey, TVal> {
+ private:
+    TMassive<TPair<TKey, TVal>> _data;
+
+ public:
+    TMassiveUnsortedTable() = default;
+    ~TMassiveUnsortedTable() override = default;
+    bool Insert(const TKey& key, const TVal& val) override;
+    TVal Find(const TKey& key) const override;
+    bool IsExists(const TKey& key) const override;
+    bool Delete(const TKey& key) override;
+    size_t GetSize() const override;
+    void Clear() override;
+};
+
+template <typename TKey, typename TVal>
+bool TMassiveUnsortedTable<TKey, TVal>::Insert(
+    const TKey& key, const TVal& val) {
+    if (IsExists(key)) {
+        return false;
+    }
+    TPair<TKey, TVal> pair(key, val);
+    _data.push_back(pair);
+    return true;
+}
+
+template <typename TKey, typename TVal>
+TVal TMassiveUnsortedTable<TKey, TVal>::Find(const TKey& key) const {
+    for (size_t i = 0; i < _data.size(); ++i) {
+        if (_data[i].first() == key) {
+            return _data[i].second();
+        }
+    }
+    throw std::runtime_error("Key not found");
+}
+
+template <typename TKey, typename TVal>
+bool TMassiveUnsortedTable<TKey, TVal>::IsExists(const TKey& key) const {
+    for (size_t i = 0; i < _data.size(); ++i) {
+        if (_data[i].first() == key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename TKey, typename TVal>
+bool TMassiveUnsortedTable<TKey, TVal>::Delete(const TKey& key) {
+    for (size_t i = 0; i < _data.size(); ++i) {
+        if (_data[i].first() == key) {
+            _data.remove_by_index(i);
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename TKey, typename TVal>
+size_t TMassiveUnsortedTable<TKey, TVal>::GetSize() const {
+    return _data.size();
+}
+
+template <typename TKey, typename TVal>
+void TMassiveUnsortedTable<TKey, TVal>::Clear() {
+    _data.clear();
+}
---
+++ tests/test_massive_table.cpp
@@ -0,0 +1,105 @@
+// Copyright 2025 atikdd.t.me.
+
+#include <massive_unsorted_table.h>
+#include <gtest/gtest.h>
+#include <string>
+
+class MassiveUnsortedTableTest : public ::testing::Test {
+ protected:
+    TMassiveUnsortedTable<int, std::string> table;
+
+    void SetUp() override {
+        table.Insert(1, "One");
+        table.Insert(2, "Two");
+        table.Insert(3, "Three");
+    }
+};
+
+TEST_F(MassiveUnsortedTableTest, InsertNewKeysReturnsTrue) {
+    EXPECT_TRUE(table.Insert(4, "Four"));
+    EXPECT_TRUE(table.Insert(5, "Five"));
+}
+
+TEST_F(MassiveUnsortedTableTest, InsertExistingKeyReturnsFalse) {
+    EXPECT_FALSE(table.Insert(1, "Another One"));
+    EXPECT_FALSE(table.Insert(2, "Another Two"));
+}
+
+TEST_F(MassiveUnsortedTableTest, FindExistingKeyReturnsValue) {
+    EXPECT_EQ(table.Find(1), "One");
+    EXPECT_EQ(table.Find(2), "Two");
+    EXPECT_EQ(table.Find(3), "Three");
+}
+
+TEST_F(MassiveUnsortedTableTest, FindNonExistingKeyThrowsException) {
+    EXPECT_THROW(table.Find(4), std::runtime_error);
+    EXPECT_THROW(table.Find(99), std::runtime_error);
+}
+
+TEST_F(MassiveUnsortedTableTest, IsExistsReturnsTrueForExistingKeys) {
+    EXPECT_TRUE(table.IsExists(1));
+    EXPECT_TRUE(table.IsExists(2));
+    EXPECT_TRUE(table.IsExists(3));
+}
+
+TEST_F(MassiveUnsortedTableTest, IsExistsReturnsFalseForNonExistingKeys) {
+    EXPECT_FALSE(table.IsExists(4));
+    EXPECT_FALSE(table.IsExists(99));
+}
+
+TEST_F(MassiveUnsortedTableTest, DeleteExistingKeyReturnsTrue) {
+    EXPECT_TRUE(table.Delete(2));
+    EXPECT_FALSE(table.IsExists(2));
+    EXPECT_EQ(table.GetSize(), 2);
+}
+
+TEST_F(MassiveUnsortedTableTest, DeleteNonExistingKeyReturnsFalse) {
+    EXPECT_FALSE(table.Delete(4));
+    EXPECT_FALSE(table.Delete(99));
+    EXPECT_EQ(table.GetSize(), 3);
+}
+
+TEST_F(MassiveUnsortedTableTest, GetSizeReturnsCorrectSize) {
+    EXPECT_EQ(table.GetSize(), 3);
+    table.Insert(4, "Four");
+    EXPECT_EQ(table.GetSize(), 4);
+    table.Delete(1);
+    EXPECT_EQ(table.GetSize(), 3);
+}
+
+TEST_F(MassiveUnsortedTableTest, ClearEmptiesTheTable) {
+    table.Clear();
+    EXPECT_EQ(table.GetSize(), 0);
+    EXPECT_FALSE(table.IsExists(1));
+    EXPECT_FALSE(table.IsExists(2));
+    EXPECT_FALSE(table.IsExists(3));
+}
+
+TEST_F(MassiveUnsortedTableTest, InsertAfterClearWorks) {
+    table.Clear();
+    EXPECT_TRUE(table.Insert(1, "One"));
+    EXPECT_EQ(table.GetSize(), 1);
+    EXPECT_TRUE(table.IsExists(1));
+}
+
+TEST_F(MassiveUnsortedTableTest, EmptyTableInitially) {
+    TMassiveUnsortedTable<int, std::string> emptyTable;
+    EXPECT_EQ(emptyTable.GetSize(), 0);
+    EXPECT_FALSE(emptyTable.IsExists(1));
+}
+
+TEST_F(MassiveUnsortedTableTest, DeleteFromEmptyTableReturnsFalse) {
+    TMassiveUnsortedTable<int, std::string> emptyTable;
+    EXPECT_FALSE(emptyTable.Delete(1));
+}
+
+TEST_F(MassiveUnsortedTableTest, FindInEmptyTableThrowsException) {
+    TMassiveUnsortedTable<int, std::string> emptyTable;
+    EXPECT_THROW(emptyTable.Find(1), std::runtime_error);
+}
+
+TEST_F(MassiveUnsortedTableTest, ClearEmptyTableDoesNothing) {
+    TMassiveUnsortedTable<int, std::string> emptyTable;
+    emptyTable.Clear();
+    EXPECT_EQ(emptyTable.GetSize(), 0);
+}
