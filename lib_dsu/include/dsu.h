bb19f06d9252c92db3ab781ff8050869cd3494c6
AtikD <luckastey@mail.ru>
2025-05-05 10:45:57 +0300

    dsu full realization

--- lib_dsu/include/dsu.h
+++ lib_dsu/include/dsu.h
@@ -1,17 +1,18 @@
 // Copyright 2025 atikdd.t.me
 #pragma once
+#include <stdexcept>
 
 class DSU {
     int _size;
     int *_parent;
-    int *rank;
+    int *_rank;
 
  public:
     explicit DSU(int size = 0);
     ~DSU();
-    // void make_set(int elem);
+    void make_set(int elem);
     int find(int elem);
     void union_trivial(int first, int second);
     void union_by_rank(int first, int second);
-    // void clear();
+    void clear();
 };
--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -1,51 +1,103 @@
 // Copyright 2025 atikdd.t.me
-
 #include <gtest/gtest.h>
 #include <dsu.h>
-#include <iostream>
 
-TEST(DSUTest, FindSuccessful) {
-    DSU a(3);
-    EXPECT_EQ(a.find(1), 1);
-    EXPECT_EQ(a.find(2), 2);
-    EXPECT_EQ(a.find(3), 3);
+TEST(DSUTest, Constructor) {
+    ASSERT_NO_THROW(DSU dsu);
+
+    ASSERT_NO_THROW(DSU dsu(5));
+
+    ASSERT_THROW(DSU dsu(-1), std::invalid_argument);
 }
 
-TEST(DSUTest, FindOutOfRange) {
-    DSU a(3);
-    EXPECT_THROW(a.find(10), std::logic_error);
-    EXPECT_THROW(a.find(-1), std::logic_error);
+TEST(DSUTest, MakeSet) {
+    DSU dsu;
+
+    ASSERT_NO_THROW(dsu.make_set(1));
+
+    ASSERT_NO_THROW(dsu.make_set(2));
+    ASSERT_NO_THROW(dsu.make_set(3));
+
+    ASSERT_THROW(dsu.make_set(3), std::invalid_argument);
+
+    ASSERT_THROW(dsu.make_set(0), std::invalid_argument);
+    ASSERT_THROW(dsu.make_set(-1), std::invalid_argument);
+}
+
+TEST(DSUTest, Find) {
+    DSU dsu(5);
+
+    ASSERT_EQ(dsu.find(1), 1);
+    ASSERT_EQ(dsu.find(2), 2);
+    ASSERT_EQ(dsu.find(3), 3);
+    ASSERT_EQ(dsu.find(4), 4);
+    ASSERT_EQ(dsu.find(5), 5);
+
+    ASSERT_THROW(dsu.find(0), std::logic_error);
+    ASSERT_THROW(dsu.find(-1), std::logic_error);
+    ASSERT_THROW(dsu.find(6), std::logic_error);
+}
+
+TEST(DSUTest, UnionTrivial) {
+    DSU dsu(5);
+
+    // Объединение двух множеств
+    dsu.union_trivial(1, 2);
+    ASSERT_EQ(dsu.find(1), dsu.find(2));
+
+    // Объединение еще двух множеств
+    dsu.union_trivial(3, 4);
+    ASSERT_EQ(dsu.find(3), dsu.find(4));
+
+    // Проверка, что элементы из разных множеств не связаны
+    ASSERT_NE(dsu.find(1), dsu.find(3));
+
+    // Объединение уже объединенных множеств
+    dsu.union_trivial(2, 1);
+    ASSERT_EQ(dsu.find(1), dsu.find(2));
+
+    // Объединение множеств из разных компонент
+    dsu.union_trivial(2, 3);
+    ASSERT_EQ(dsu.find(1), dsu.find(3));
+    ASSERT_EQ(dsu.find(1), dsu.find(4));
+}
+
+TEST(DSUTest, UnionByRank) {
+    DSU dsu(7);
+
+    // Создаем дерево с корнем 1 и высотой 1
+    dsu.union_by_rank(1, 2);
+    dsu.union_by_rank(1, 3);
+
+    // Создаем дерево с корнем 4 и высотой 2
+    dsu.union_by_rank(4, 5);
+    dsu.union_by_rank(4, 6);
+    dsu.union_by_rank(6, 7);
+
+    // После объединения дерево с корнем 1 должно стать подмножеством
+    // дерева с корнем 4
+    dsu.union_by_rank(1, 4);
+
+    // Проверяем, что все элементы находятся в одном множестве
+    ASSERT_EQ(dsu.find(1), dsu.find(4));
+    ASSERT_EQ(dsu.find(2), dsu.find(4));
+    ASSERT_EQ(dsu.find(3), dsu.find(4));
+    ASSERT_EQ(dsu.find(5), dsu.find(4));
+    ASSERT_EQ(dsu.find(6), dsu.find(4));
+    ASSERT_EQ(dsu.find(7), dsu.find(4));
 }
 
 
+TEST(DSUTest, Clear) {
+    DSU dsu(5);
 
-TEST(DSUTest, UnionTrivialSuccessful) {
-    DSU a(5);
-    a.union_trivial(2, 4);
-    EXPECT_EQ(a.find(2), 2);
-    EXPECT_EQ(a.find(4), 2);
-}
+    dsu.union_trivial(1, 2);
+    dsu.union_trivial(3, 4);
 
-TEST(DSUTest, UnionTrivialIdempotency) {
-    DSU a(5);
-    a.union_trivial(2, 4);
-    a.union_trivial(4, 2);
-    EXPECT_EQ(a.find(2), 2);
-    EXPECT_EQ(a.find(4), 2);
-}
+    dsu.clear();
 
-
-TEST(DSUTest, UnionByRankSuccessful) {
-    DSU a(5);
-    a.union_by_rank(2, 4);
-    EXPECT_EQ(a.find(2), 2);
-    EXPECT_EQ(a.find(4), 2);
-}
-
-TEST(DSUTest, UnionByRankIdempotency) {
-    DSU a(5);
-    a.union_by_rank(2, 4);
-    a.union_by_rank(4, 2);
-    EXPECT_EQ(a.find(2), 2);
-    EXPECT_EQ(a.find(4), 2);
+    // После очистки размер должен быть 0
+    // и добавление нового элемента должно работать
+    ASSERT_NO_THROW(dsu.make_set(1));
+    ASSERT_EQ(dsu.find(1), 1);
 }
